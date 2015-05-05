aec7779244c7e3e4c344a2998a4dbf94425537e1
Shriansh Shrivastava <shriansh@gmail.com>
2015-05-04 21:13:06 -0500

    today in class

--- app/controllers/zodiacs_controller.rb
+++ app/controllers/zodiacs_controller.rb
@@ -1,3 +1,45 @@
 class ZodiacsController < ApplicationController
 
+
+  def random_number
+
+    @num1 = params[:the_number]
+    @num2 = params[:second_number]
+
+    render 'dynamic'
+
+  end
+
+
+  def sign
+    @my_sign = params[:the_sign]
+    @fortune = Zodiac.find_by({:sign => @my_sign})
+    render 'dynamic_signs'
+
+  end
+
+  def creature
+    @my_creature = params[:the_creature]
+    @fortune = Zodiac.find_by({:creature => @my_creature})
+    render 'dynamic_signs'
+
+  end
+
+
+  def id
+    @my_id = params[:the_id]
+    @fortune = Zodiac.find_by({:id => @my_id})
+    render 'dynamic_signs'
+
+  end
+
+
+  def index
+    @list_of_zodiacs = Zodiac.all
+    render 'index'
+  end
+
+def new_form
+
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,17 @@ Rails.application.routes.draw do
   get("/lucky_numbers",   { :controller => "fortunes", :action => "lucky" })
   get("/unlucky_numbers", { :controller => "fortunes", :action => "unlucky" })
 
+
+  get("/random/:the_number/:second_number", { :controller => "zodiacs", :action => "random_number"})
+
   # VARIABLE ROUTES
   # =======================
 
-  # get("/signs/:the_sign",         { :controller => "zodiacs", :action => "sign" })
-  # get("/creatures/:the_creature", { :controller => "zodiacs", :action => "creature" })
+  get("/signs/:the_sign",         { :controller => "zodiacs", :action => "sign" })
+  get("/creatures/:the_creature", { :controller => "zodiacs", :action => "creature" })
+
+  get("/zodiacs/:new", {:controller => "zodiacs", :action => "new_form"})
+
+  get("/zodiacs/:the_id", {:controller => "zodiacs", :action => "id"})
+  get("/zodiacs", {:controller => "zodiacs", :action => "index"})
 end
